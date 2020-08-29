1910dc7c74c1aa3a15b0fcb6237d071198ba8ed2
mehedi rana <mdmehedirana@gmail.com>
2020-08-30 00:13:45 +0600

    auth successful

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { Entypo } from '@expo/vector-icons';
+import * as Facebook from 'expo-facebook';
 import * as Google from 'expo-google-app-auth';
 import * as firebase from "firebase";
+
 
 class SignInScreen extends Component {
 
@@ -14,24 +16,34 @@ class SignInScreen extends Component {
     err: null,
   };
   
+  // componentDidMount=()=>{
+  //   firebase.auth.OAuthCredential((user)=>{
+  //     if(user!=null){
+  //       console.log(user)
+  //     }
+  //   })
+  // }
 
     signInWithGoogleAsync = async ()=> {
       try {
         const result = await Google.logInAsync({
+          //behavior:'web',
           androidClientId: "383958588869-43lj98ut80fa4djid3clavkt36uhj22a.apps.googleusercontent.com",
         //  iosClientId: YOUR_CLIENT_ID_HERE,
           scopes: ['profile', 'email'],
           
         });
-        this.props.navigation.navigate('Home');
+        
         if (result.type === 'success') {
-          
+        //  this.props.navigation.navigate('Home');
+        alert("logIn")
           console.log("//////////" +result.accessToken)  
           return result.accessToken;
         } else {
           return { cancelled: true };
         }
       } catch (e) {
+        alert(e)
         console.log("error : "+e)
         return { error: true };
       }
@@ -40,8 +52,32 @@ class SignInScreen extends Component {
    
 
 
-  facebookLogIn = () =>{
-
+  facebookLogIn = async() =>{
+    
+      try {
+        await Facebook.initializeAsync('928520384308033');
+        const {
+          type,
+          token,
+          expires,
+          permissions,
+          declinedPermissions,
+        } = await Facebook.logInWithReadPermissionsAsync({
+          permissions: ['public_profile'],
+        });
+        if (type === 'success') {
+          console.log("\\\\\\success")
+          // Get the user's name using Facebook's Graph API
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+          alert('Logged in!', `Hi ${(await response.json()).name}!`);
+          this.props.navigation.navigate('Home');
+        //  this.props.navigation.navigate('Home');
+        } else {
+          // type === 'cancel'
+        }
+      } catch ({ message }) {
+        alert(`Facebook Login Error: ${message}`);
+      }
   }
 
   handleLogin = () => {
@@ -105,10 +141,10 @@ class SignInScreen extends Component {
           <Text style={{ color: "#8A8F9E", marginTop: 10, alignSelf: "center", fontWeight: "bold" }}>OR LOGING USING</Text>
           <View style={{ flexDirection: "row",justifyContent:'space-between', marginTop:10, }}>
 
-            <TouchableOpacity style={{marginLeft:120}} onPress={this.facebookLogIn}>
+            <TouchableOpacity style={{marginLeft:120}} onPress={()=>this.facebookLogIn()}>
               <Entypo name="facebook-with-circle" size={35} color="#8A8F9E" />
             </TouchableOpacity>
-            <TouchableOpacity style={{marginRight:120}} onPress={this.signInWithGoogleAsync}>
+            <TouchableOpacity style={{marginRight:120}} onPress={()=>this.signInWithGoogleAsync()}>
               <Entypo name="google--with-circle" size={35} color="#8A8F9E" />
             </TouchableOpacity>
 
